361943f52bc27d7446d86d321a1c6e54634daa44
Abayomi Aishida <yomicorperate@gmail.com>
2024-10-19 18:34:20 +0100

    create and display account info using wagmi hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import PageWithNavbar from "@/components/layout/pageWithNavbar";
-// import { Account } from '@/components/web3/account';
-// import { ConnectKitButton } from 'connectkit';
+import { Account } from "@/components/web3/account";
+import { ConnectKitButton } from "connectkit";
 
 export default function Home() {
   return (
@@ -11,8 +11,8 @@ export default function Home() {
         <div className="container md:pt-4 lg:pt-12 xl:pt-20">
           <h1 className="mb-4 text-6xl">EtherStart</h1>
           <div className="py-8 space-y-4 w-full flex flex-col items-center">
-            {/* <ConnectKitButton />
-            <Account /> */}
+            <ConnectKitButton />
+            <Account />
           </div>
         </div>
       </div>
---
+++ src/components/web3/account.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useAccount, useBalance, useEnsAvatar, useEnsName } from "wagmi";
+import { useEffect, useState } from "react";
+import { mainnet } from "viem/chains";
+import Image from "next/image";
+
+export function Account() {
+  const [isMounted, setIsMounted] = useState(false);
+  const { address, chain, chainId, isConnected } = useAccount();
+  const accountBalance = useBalance({
+    address,
+  });
+
+  const { data: ensName } = useEnsName({
+    address,
+    chainId: mainnet.id,
+  });
+
+  const { data: ensAvatar } = useEnsAvatar({
+    name: ensName!,
+    chainId: mainnet.id,
+  });
+
+  useEffect(() => {
+    if (!isMounted) {
+      setIsMounted(true);
+    }
+  }, [isMounted]);
+
+  if (!isConnected) {
+    return (
+      <div>
+        <p className="text-lg">Not connected</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center text-center gap-y-4">
+      {ensAvatar && ensName && isMounted && (
+        <div className="flex items-center gap-x-2">
+          <Image
+            alt="ENS Avatar"
+            src={ensAvatar}
+            className="h-16 w-16 rounded-full"
+            height={64}
+            width={64}
+          />
+          {ensName && <p className="text-2xl">{ensName}</p>}
+        </div>
+      )}
+      {address && isMounted && (
+        <>
+          <p className="text-lg">{address}</p>
+        </>
+      )}
+      <div className="flex flex-col gap-y-2">
+        {accountBalance && (
+          <p className="text-xl">
+            Balance: {accountBalance.data?.formatted} ETH
+          </p>
+        )}
+        {chain && chainId && isMounted && (
+          <p className="text-lg">
+            {chain.name}, chainId: {chainId}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
